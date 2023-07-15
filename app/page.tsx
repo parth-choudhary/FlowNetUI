eb66691cd97ad62edd8873a144cbe8b1127bc14f
Parth Choudhary <parth.choudhary@gmail.com>
2023-07-15 18:19:21 +0530

    changes for swag

--- app/page.tsx
+++ app/page.tsx
@@ -17,7 +17,7 @@ export default function Home() {
 
   const [user, setUser] = useState({loggedIn: null, addr: null})
   const [nodeList, setNodeList] = useState([{name: '123', addr: '123'}, {name: '123', addr: '123'}, {name: '123', addr: '123'}, {name: '123', addr: '123'}]);
-  const [node, setNode] = useState(''); // nodeList[0
+  const [node, setNode] = useState({name: '123', addr: '123'}); // nodeList[0
   const [selectedNode, setSelectedNode] = useState(null);
   const [processing, setProcessing] = useState(false);
   const [value, setValue] = useState('');
@@ -25,18 +25,116 @@ export default function Home() {
   const [showImage, setShowImage] = useState(false);
   const [image, setImage] = useState('https://dummyimage.com/250/ffffff/000000');
   const [images, setImages] = useState([{link:'https://dummyimage.com/250/ffffff/000000', description:'some description'},{link:'https://dummyimage.com/250/ffffff/000000', description:'some description'},{link:'https://dummyimage.com/250/ffffff/000000', description:'some description'},{link:'https://dummyimage.com/250/ffffff/000000', description:'some description'},{link:'https://dummyimage.com/250/ffffff/000000', description:'some description'}]);  
+  const [txid, setTxid] = useState(null);
 
+  useEffect(() => {
+    if(txid){
+      const interval = setInterval(() => {
+        fcl.tx(txid).onceSealed().then(() => {
+          // fetchImages();
+          setTxid(null);
+          setProcessing(false);
+        });
+      }, 1000);
+      return () => clearInterval(interval);
+    }
+  }, [txid]);
+
+  const startInference = async () => {
+    setProcessing(true);
+    const result = await fcl.mutate({
+      cadence: `
+      import MainContract from 0xf8d6e0586b0a20c7
+      import ExampleToken from 0xf8d6e0586b0a20c7
+      import FungibleToken from "FungibleToken"
+
+      transaction(recipient: Address){ //type: String, url: String
+
+          let sender: @ExampleToken.Vault
+          let vault: Capability //<&ExampleToken.Vault{FungibleToken.Receiver}>
+          let tokenReceiver: &{FungibleToken.Receiver}
+
+
+          prepare(signer: AuthAccount){
+
+              self.sender <- signer.borrow<&ExampleToken.Vault>(from: ExampleToken.VaultStoragePath)!.withdraw(amount: UFix64(1)) as! @ExampleToken.Vault
+
+              var account = getAccount(0xf8d6e0586b0a20c7)
+              self.tokenReceiver = account
+                  .getCapability(ExampleToken.ReceiverPublicPath)
+                  .borrow<&{FungibleToken.Receiver}>()
+                  ?? panic("Unable to borrow receiver reference")
+
+              self.vault = signer.getCapability(ExampleToken.ReceiverPublicPath)
+
+          }
+          execute{
+              MainContract.requestInference(
+                  prompt: "${prompt}", 
+                  requestor: ${user.addr},
+                  responder: ${node.addr},
+                  offer: 1,
+                  requestorVault: <- self.sender,
+                  receiverCapability: self.tokenReceiver
+              )
+          }
+      }
+      `,
+      args: (arg, t) => [],
+    });
+    console.log(result); // 
+  };
+
+  const fetchImages = async () => {
+    const result = await fcl.query({
+      cadence: `
+        import MainContract from 0x0fb46f70bfa68d94
+        pub fun main(): {UInt64: MainContract.Response} {           
+          return MainContract.getResponses()
+        }
+      `
+      });
+    console.log('images', result); // 1
+    const keys = Object.keys(result);
+    const values: any = Object.values(result);
+    const resultArray = [];
+    for(let i = 0; i < keys.length; i++){
+      resultArray.push({link: values[i].url, description: values[i].link.split('=')[1]});
+    }
+    setImages(resultArray);
+  };
+
+  const fetchNodes = async () => {
+    const result = await fcl.query({
+      cadence: `
+        import MainContract from 0x0fb46f70bfa68d94
+        pub fun main(): {Address: MainContract.Responder} {        
+            return MainContract.getResponders()
+        }
+      `,
+      // args: (arg, t) => null,
+    });
+    console.log(result); // 1
+    // convert result to array
+    const keys = Object.keys(result);
+    const values = Object.values(result);
+    const resultArray = [];
+    for(let i = 0; i < keys.length; i++){
+      resultArray.push({name: keys[i], addr: keys[i], data: values[i]});
+    }
+    setNodeList(resultArray);
+  }
 
   useEffect(() => {fcl.currentUser.subscribe(setUser)}, []);
 
   useEffect(() => {
     //fetch nodelist here and set to setNodeList
-    // TODO: fetch nodelist from server
+    fetchNodes();
+    fetchImages();
   }, [])
-  console.log(user);
 
   const nodeSelectionList = nodeList.map((node: any, i) => {
-    console.log(node);
+    // console.log(node);
     return (
       <RadioTile style={{width: '80vw'}} key={`node-list-${i}`}  icon={<Icon as={VscWorkspaceTrusted} />} label={`${node.name}`} value={`${node.name}-${i}`}>
         Address: {node.addr}
@@ -101,7 +199,7 @@ export default function Home() {
                 <RadioTileGroup onChange={(e) => setNode(e.toString())} defaultValue="private" aria-label="Visibility Level">
                   {nodeSelectionList}
                 </RadioTileGroup>
-                <Button className='hover' appearance="primary" color="yellow" style={{padding: 12, fontSize: 20, marginTop: 16, fontWeight: 600,  width: '100%', background: '#2F476B', borderRadius: 42}} onClick={() => {setProcessing(true); setSelectedNode(node); triggerProcessingDone();}}>Select</Button>
+                <Button className='hover' appearance="primary" color="yellow" style={{padding: 12, fontSize: 20, marginTop: 16, fontWeight: 600,  width: '100%', background: '#2F476B', borderRadius: 42}} onClick={() => {setProcessing(true); setSelectedNode(node); startInference();}}>Select</Button>
               </div>
               </> : <h4 style={{marginTop: 100, fontSize: 22, fontWeight: 400, display: 'flex', alignSelf: 'start'}}>
                 {/* Selected Node: <span style={{marginLeft: 8, fontWeight: 800}}>{selectedNode}</span> */}
@@ -111,7 +209,7 @@ export default function Home() {
           {prompt == '' && !processing &&  user.loggedIn && <div style={{marginTop: 10, width: '100%'}}>
             <div style={{fontSize: 24, marginBottom: 12}}>Please enter a prompt to create a AI generation</div>
             <Input as="textarea" rows={3} placeholder="Please enter prompt..." onChange={(value) => {setValue(value)}}/>
-            <Button onClick={() => {setPrompt(value)}} className='hover' appearance="primary" color="yellow" style={{fontSize: 20, padding: 12, marginTop: 10, fontWeight: 600,  width: '100%', background: '#2F476B', borderRadius: 42}}>Submit</Button>
+            <Button onClick={() => {setPrompt(value);}} className='hover' appearance="primary" color="yellow" style={{fontSize: 20, padding: 12, marginTop: 10, fontWeight: 600,  width: '100%', background: '#2F476B', borderRadius: 42}}>Submit</Button>
           </div>}
           </> : <>
             <Button className='hover cta-button' style={{margin: 4, marginTop: 200, padding: 12, paddingLeft: 32, paddingRight: 32}} onClick={fcl.logIn}>LOG IN TO PROCEED</Button>
@@ -123,24 +221,28 @@ export default function Home() {
             </div>
           }
       </main>
-      <Ticker offset="run-in" speed={5} >
-          {({ index: any }) => (
-              <>
-                <div style={{ whiteSpace: "nowrap", display: 'flex', margin: 1 }}>
-                  {gallery}
-                </div>
-              </>
-          )}
-        </Ticker>
-        <Ticker offset="run-in" speed={5} direction="toRight" >
-          {({ index: any }) => (
-              <>
-                <div style={{ whiteSpace: "nowrap", display: 'flex', margin: 1 }}>
-                  {gallery}
-                </div>
-              </>
-          )}
-        </Ticker>
+      {images.length > 0 && <>
+        <Ticker offset="run-in" speed={5} >
+            {({ index: any }) => (
+                <>
+                  <div style={{ whiteSpace: "nowrap", display: 'flex', margin: 4 }}>
+                    {gallery}
+                  </div>
+                </>
+            )}
+          </Ticker>
+          <br></br>
+          <Ticker offset="run-in" speed={5} direction="toRight" >
+            {({ index: any }) => (
+                <>
+                  <div style={{ whiteSpace: "nowrap", display: 'flex', margin: 4 }}>
+                    {gallery}
+                  </div>
+                </>
+            )}
+          </Ticker>
+          </>
+        }
     </div>
   )
 }
